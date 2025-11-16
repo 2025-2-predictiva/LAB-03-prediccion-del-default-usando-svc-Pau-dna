de3a46cecdfdda75737e07956b5ea2aea34f2e1a
Paula Suarez <161689914+Pau-dna@users.noreply.github.com>
2025-11-15 21:54:56 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -47,7 +47,7 @@ METRICS = [
     {
         "type": "cm_matrix",
         "dataset": "test",
-        "true_0": {"predicted_0": 6710, "predicted_1": None},
+        "true_0": {"predicted_0": 6700, "predicted_1": None},
         "true_1": {"predicted_0": None, "predicted_1": 720},
     },
 ]
